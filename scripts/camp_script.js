f4cb1c6a4945787c93ddf5dd29dd2cbbee8a3ce3
Mark <wixw1cky7@gmail.com>
2025-02-21 20:36:24 +0700

    added campaign creation
    - campaign creation webapp added
    - made auth look better

---
+++ scripts/camp_script.js
@@ -0,0 +1,29 @@
+document.addEventListener("DOMContentLoaded", function () {
+    Telegram.WebApp.expand();
+
+    const form = document.getElementById("adForm");
+    
+    form.addEventListener("submit", function (event) {
+        event.preventDefault();
+
+        const formData = {
+            impressions_limit: parseInt(document.getElementById("impressions_limit").value) || null,
+            clicks_limit: parseInt(document.getElementById("clicks_limit").value) || null,
+            cost_per_impression: parseFloat(document.getElementById("cost_per_impression").value) || null,
+            cost_per_click: parseFloat(document.getElementById("cost_per_click").value) || null,
+            ad_title: document.getElementById("ad_title").value || null,
+            ad_text: document.getElementById("ad_text").value || null,
+            start_date: parseInt(document.getElementById("start_date").value) || null,
+            end_date: parseInt(document.getElementById("end_date").value) || null,
+            targeting: {
+                gender: document.getElementById("gender").value || null,
+                age_from: parseInt(document.getElementById("age_from").value) || null,
+                age_to: parseInt(document.getElementById("age_to").value) || null,
+                location: document.getElementById("location").value || null,
+            }
+        };
+
+        Telegram.WebApp.sendData(JSON.stringify(formData));
+        Telegram.WebApp.close()
+    });
+});
